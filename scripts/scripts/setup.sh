5737562db7076baa68e6c691ef2ff7452496a0f1
falenn <dev@imwiz.com>
2018-09-06 19:10:25 +0000

    Added stow feature.

---
+++ scripts/scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# initial software to load
+sudo yum install -y git stow ansible
+
+# From here, lets run some ansible playbooks
+
---
+++ scripts/scripts/stowAll.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/dotfiles
+
+rm -rf ~/.bash*
+
+stow bash
+. ~/.bash_profile
+
+rm -rf ~/.git
+stow git
+
