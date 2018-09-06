f3f1b75a8feb99a97259583c350310921c8a0aa9
falenn <dev@imwiz.com>
2018-09-06 19:14:55 +0000

    Added xrdp install

---
+++ scripts/scripts/setupXRDP.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo yum groupinstall -y "X Window System"
+sudo yum groupinstall -y "Desktop Fonts"
+sudo yum install gnome-classic-session gnome-terminal nautilus-open-terminal control-center liberation-mono-fonts
+
+sudo yum groupinstall -y "General Purpose Desktop"
+sudo yum groupinstall -y --skip-broken "GNOME Desktop"
+
+sudo yum install -y firefox
+
+sudo yum install -y xrdp
+
+sudo chkconfig --levels 5 xrdp on
+
+sudo ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+
