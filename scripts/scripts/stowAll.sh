86f5059ad6b1ac271cb3a01fdb74d69ac0bf443d
falenn <dev@imwiz.com>
2018-09-12 16:15:59 +0000

    Added /data helper to stowAll.sh

--- scripts/scripts/stowAll.sh
+++ scripts/scripts/stowAll.sh
@@ -11,3 +11,15 @@ stow scripts
 rm -rf ~/.bash*
 stow bash
 . ~/.bashrc
+
+if [ -e /data ]; then
+ echo "Data directory exists.  linking dirs"
+ ln -s /data/apps ~/apps
+ ln -s /data/dev  ~/dev
+ ln -s /data/.gradle ~/.gradle
+ ln -s /data/.m2 ~/.m2
+ if [ -e ~/Downloads ]; then
+    rm -rf ~/Downloads
+ fi
+ ln -s /data/Downloads ~/Downloads
+fi
