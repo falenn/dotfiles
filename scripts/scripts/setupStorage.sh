fb0cd051c442ddf516353c11de7046ad89ae33ab
falenn <dev@imwiz.com>
2018-12-20 22:16:12 +0000

    skeleton script.  fix

--- scripts/scripts/setupStorage.sh
+++ scripts/scripts/setupStorage.sh
@@ -31,7 +31,6 @@ function mountDevice() {
   sudo /bin/mount $1
 }
 
-
 OUT=`ls /dev | grep -e "$DEV_NAME"`
 if [[ $? -eq 1 ]]; then
   if [ "$RESTART" == "y" ]; then
@@ -49,4 +48,42 @@ echo "Device exists. Continuing.."
 
 sudo /bin/mount | grep -e "$MOUNT_NAME"
 if [[ $? -eq 1 ]]; then
-  echo "Storage is not partitioned.
+  echo "Storage is not partitioned."
+
+ -- partition it
+
+
+# create parition volume
+sudo /sbin/pvcreate -d /dev/"$DEV_NAME""1"
+
+# display volume group
+sudo vgdisplay
+
+# create the volume group
+sudo /sbin/vgcreate $VOLGROUPNAME /dev/"$DEV_NAME""1"
+
+# scan for existing logical volumes
+sudo lvscan
+
+#create the logicalvolume in the group
+sudo /sbin/lvcreate -l 100%VG $VOLGROUPNAME -n $VOLUMENAME
+
+# display volume groups
+sudo lvdisplay -v /dev/"$DEV_NAME""1"
+
+# format logical volume
+sudo /sbin/mkfs.ext4 /dev/$VOLGROUPNAME/$VOLUMENAME
+
+# make mount location, a.k.a /data
+mkdir -p $MOUNT_PATH
+
+cat /etc/fstab | grep /dev/$VOLGROUPNAME/$VOLUMENAME
+if [ $? -eq 1 ]; then
+  # not in fstab.  write entry.  have to use spec thing for sudo into root-owned file
+echo " /dev/$VOLGROUPNAME/$VOLUMENAME $MOUNT_NAME ext4 defaults 0 0"
+
+sudo /bin/mount -a
+
+#change perms so you can write to the storage
+sudo chmod ugoa+rx $MOUNT_NAME
+
