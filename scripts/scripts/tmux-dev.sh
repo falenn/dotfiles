28e64e2ffb8a0a10a1f0e2e2d1c1f86988e5c7f1
falenn <dev@imwiz.com>
2019-08-16 17:55:57 +0000

    Added aws helpers and tmux fix

--- scripts/scripts/tmux-dev.sh
+++ scripts/scripts/tmux-dev.sh
@@ -7,15 +7,14 @@ tmux has-session -t $SESSIONNAME &> /dev/null
 
 if [ $? != 0 ]; then
 	tmux new-session -s $SESSIONNAME -d
-
+   
 	for i in 0 1
 	do
-	  tmux split-window -v -t $WINDOW.$i
-	done
-	
-	tmux select-layout -t $SESSIONNAME tiled
-
-	tmux send-keys -t $WINDOW.0 'cd ~/dev' C-m
+	  tmux split-window -v -t $WINDOW.$i -c '~/dev'
+	  tmux send-keys -t $WINDOW.$i C-z Enter
+        done
+        tmux send-keys -t $WINDOW.2 C-z Enter
+        tmux select-layout -t $SESSIONNAME tiled
 fi
 
 tmux attach -t $SESSIONNAME
