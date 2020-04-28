14fcc11d0692a795e799bf79f67904d132b3e42e
falenn <dev@imwiz.com>
2020-04-28 14:24:28 -0400

    updates

--- scripts/scripts/installDocker.sh
+++ scripts/scripts/installDocker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DOCKER_VER=19.*
+
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
@@ -7,31 +9,35 @@ sudo yum-config-manager \
 sudo yum-config-manager --disable docker-ce-edge
 sudo yum-config-manager --disable docker-ce-test
 
-sudo yum install -y docker-ce
+sudo yum install -y docker-ce-$DOCKER_VER
 
 # Docker config
 sudo mkdir -p /etc/docker
 cat << 'EOF' | sudo tee /etc/docker/daemon.json
 { 
   "storage-driver":"devicemapper",
-  "exec-opts": ["native.cgroupdriver=cgroupfs"],
+  "exec-opts": ["native.cgroupdriver=systemd"],
   "data-root": "/data/docker",
   "log-driver":"json-file",
   "log-opts":{
     "max-size":"10m",
     "max-file": "2"
   },
-  "dns": ["10.110.69.207"]
+  "dns": ["8.8.8.8"]
 }
 EOF
 
 
 # docker tab completion
-curl -L https://raw.githubusercontent.com/docker/docker-ce/v$(docker -v | cut -d' ' -f3 | tr -d ',')/components/cli/contrib/completion/bash/docker | sudo tee /etc/bash_completion.d/docker
+#curl -L https://raw.githubusercontent.com/docker/docker-ce/v$(docker -v | cut -d' ' -f3 | tr -d ',')/components/cli/contrib/completion/bash/docker | sudo tee /etc/bash_completion.d/docker
 
 # docker compose completion
-curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose | sudo tee /etc/bash_completion.d/docker-compose
+#curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose | sudo tee /etc/bash_completion.d/docker-compose
+
+# disable swap
+sudo swapoff -a
 
 # start the service
+sudo systemctl enable docker
 sudo systemctl start docker
 
